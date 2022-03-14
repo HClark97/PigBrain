f4f783853495a493f68365ca319d6468a6d38702
Mikkel Bjerre <mbj18@student.aau.dk>
2022-03-14 09:08:47 +0100

    pdf plot

--- loader.py
+++ loader.py
@@ -1,3 +1,18 @@
 import tdt
 
-data = tdt.read_block('Y:\Data\Pig Work\Suzan\Fall 2021\Chronic cortical data\Suzan_Chronic2-211102')
+data = tdt.read_block('Z:\Data\Pig Work\Suzan\Spring 2021\Student data\Subject9-210426-124955')
+Fs = data.streams.RSn1.fs
+data = data.streams.RSn1.data
+import numpy as np
+from scipy import signal
+import scipy.fftpack 
+from scipy.fft import fft, fftfreq
+import matplotlib.pyplot as plt
+
+f,pxx = scipy.signal.periodogram(data, fs=Fs, window='boxcar', nfft=None, detrend='constant', return_onesided=True, scaling='density', axis=- 1)
+pxx = 10*np.log10(pxx)
+plt.plot(f,pxx)
+plt.grid()
+plt.xlim(0,100)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Amplitude (dB/Hz)")
