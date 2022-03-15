dab4c7d40dd98e9ff4b5896a7a9de51366c0c9b6
Nickolaj <nickolaj.ajay@gmail.com>
2022-03-15 13:34:37 +0100

    Added loader function

--- loader.py
+++ loader.py
@@ -1,20 +1,39 @@
-import tdt
+import scipy.io
+import h5py as h5py
 import numpy as np
-from scipy import signal
-import scipy.fftpack 
-from scipy.fft import fft, fftfreq
-import matplotlib.pyplot as plt
-import FunctionsP7.FFT.Power_density as pdf
+
+def load_mat_file(path):
+    """
+    Parameters
+    ----------
+    path : string
+        DESCRIPTION.
+
+    Returns
+    -------
+    Non : dict
+        Contains non-noxious data
+    Nox : dict
+        Contains non-noxious data
+    
+    Help
+    -------
+    Data is accesed using the following syntax:
+    dict['block'][i]['channel'][i]['ERPs'][i]
+    
+    """
+    f = scipy.io.loadmat(path,simplify_cells=True)
+    f.pop('__globals__')
+    f.pop('__header__')
+    f.pop('__version__')
+    data = np.array(list(f.items()))
+    Non=data[0,1]
+    Nox=data[1,1]
+    
+    return Non,Nox
+    
 
 
-#load data
-data = tdt.read_block(r'C:\Users\mbj\Desktop\Uni\8. semester\Projekt\Data\Subject9-210426-124955')
-fs = data.streams.RSn1.fs
-data = data.streams.RSn1.data
-
-
-#Plot PDF
-pdf.psd_plot(data[1],fs,300)
 
 
 
