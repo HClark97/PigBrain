270029c40ba759ef14b3d85c22ba8e75022b9486
Mikkel Bjerre <mbj18@student.aau.dk>
2022-03-17 12:32:15 +0100

    prep_epoch_preprocessing

--- Preprocessing_start.py
+++ Preprocessing_start.py
@@ -11,38 +11,29 @@ import FunctionsP7.filtering.filters as butfilt
 #load data
 data = tdt.read_block(r'C:\Users\mbj\Desktop\Uni\8. semester\Projekt\Data\Subject9-210526-093759')
 
-#notchdata = tdt.digitalfilter(data,'RSn1',np.array([10,100]),'NOTCH',np.array([50,100,150,200,250,300,350]))
+
 fs = data.streams.RSn1.fs
 data = data.streams.RSn1.data
+
+
 
 #Plot psd
 fig2 = plt.figure()
 pdf.psd_plot(data[2],fs,400)
 
-
-#Harmonic notch filtering
-harmonics = np.array([50,100,150,200,250,300,350])
-notch_data = np.zeros([32,2])
-notch_data = butfilt.filt_filt_nocth_harmonic(data[0:1],harmonics, fs)
-fig3 = plt.figure()
-pdf.psd_plot(notch_data[0], fs,400)
-
-
-# If notch filter is to much, use the stopband filter below
-# stop_data = butfilt.filt_filt_but_harmonic(data[0], 'stop', 1, harmonics, fs)
-# fig = plt.figure()
-# pdf.psd_plot(stop_data, fs,300)
-
-#thrid try
-#stop_data = butfilt.butter_bandstop_filter(data[0], 49, 51, fs, 5)
-#fig = plt.figure()
-#pdf.psd(stop_data, fs)
-#Plot the results
-# fig = plt.figure()
-# pdf.psd_plot(notch_data[0],fs,400)
-
-
-
+non/nox, sets,channel, stim/epoch
+for nox in range(np.shape(files))
+    for sets in range(np.shape(nox)
+        for channel in range(np.shape(sets[0]))
+            for epoch in range(np.shape(channel[0]))
+## Baseline correction fra P7
+# baseline = np.mean(data[exp,channel,epoch]) #Taking the first 50 ms of the data for baseline correction for exp, channel and epoch
+# # Correct the baseline
+# data[exp,channel,epoch] = data[exp,channel,epoch] - baseline
+## filter the data
+# harmonics = np.array([50,100,150,200,250,300,350])
+# data[exp,channel,epoch] = f.filt_filt_nocth_harmonic(data[exp,channel,epoch],harmonics, fs)
+# data[exp,channel,epoch] = f.filt_filt_but(data[exp,channel,epoch], 'band', 5, [1,250], fs) #10 order effective lowpass filter at 400 hz
 
 
 
