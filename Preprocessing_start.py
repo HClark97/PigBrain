4a6d4f794550cdd431bcd9b9e5ba5e183ee8f8a9
Mikkel Bjerre <mbj18@student.aau.dk>
2022-03-17 12:52:44 +0100

    Preprocessing_updated

--- Preprocessing_start.py
+++ Preprocessing_start.py
@@ -15,25 +15,23 @@ data = tdt.read_block(r'C:\Users\mbj\Desktop\Uni\8. semester\Projekt\Data\Subjec
 fs = data.streams.RSn1.fs
 data = data.streams.RSn1.data
 
-
-
 #Plot psd
 fig2 = plt.figure()
 pdf.psd_plot(data[2],fs,400)
 
 non/nox, sets,channel, stim/epoch
-for nox in range(np.shape(files))
-    for sets in range(np.shape(nox)
-        for channel in range(np.shape(sets[0]))
-            for epoch in range(np.shape(channel[0]))
-## Baseline correction fra P7
-# baseline = np.mean(data[exp,channel,epoch]) #Taking the first 50 ms of the data for baseline correction for exp, channel and epoch
-# # Correct the baseline
-# data[exp,channel,epoch] = data[exp,channel,epoch] - baseline
-## filter the data
-# harmonics = np.array([50,100,150,200,250,300,350])
-# data[exp,channel,epoch] = f.filt_filt_nocth_harmonic(data[exp,channel,epoch],harmonics, fs)
-# data[exp,channel,epoch] = f.filt_filt_but(data[exp,channel,epoch], 'band', 5, [1,250], fs) #10 order effective lowpass filter at 400 hz
+for nox in range(np.size([Non,Nox]))
+    for sets in range(np.shape(Nox['block']))
+        for channel in range(np.shape(Nox['block'][0]['channel']))
+            for epoch in range(np.size(Nox['block'][0]['channel'][0]['ERPs']))
+                ## Baseline correction fra P7
+                # baseline = np.mean(data[nox,sets,channel,epoch]) #Taking the first 50 ms of the data for baseline correction for exp, channel and epoch
+                # # Correct the baseline
+                # data[nox,sets,channel,epoch] = data[exp,channel,epoch] - baseline
+                ## filter the data
+                # harmonics = np.array([50,100,150,200,250,300,350])
+                # data[nox,sets,channel,epoch] = f.filt_filt_nocth_harmonic(data[exp,channel,epoch],harmonics, fs)
+                # data[nox,sets,channel,epoch] = f.filt_filt_but(data[exp,channel,epoch], 'band', 5, [1,250], fs) #10 order effective lowpass filter at 400 hz
 
 
 
