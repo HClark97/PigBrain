636b203532f9661eb79f5ff35e6f37ecd992c8fd
amaliekoch <akan18@student.aau.dk>
2022-03-25 13:28:57 +0100

    visual inspection done

--- visual_inspection.py
+++ visual_inspection.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
+## THIS FILE IS USED FOR VISUAL INSPECTION OF THE DATA 
+# Aim: to identify noisy channels
 
 import tdt
 import numpy as np
 from scipy import signal
-import scipy.fftpack 
-from scipy.fft import fft, fftfreq
-import matplotlib.pyplot as plt
-import FunctionsP7.FFT.Power_density as pdf
 import FunctionsP7.filtering.filters as f
 import FunctionsP7.filtering.plot_data as pd
 from numpy.fft import rfft, rfftfreq
@@ -15,31 +12,31 @@ import Preprocessing.plot as p
 fontsize_title = 25
 fontsize_labels = 15
 
-#load data
+#LOAD DATA (full experiment)
 #data = tdt.read_block(r'C:\Users\mbj\Desktop\Uni\8. semester\Projekt\Data\Subject9-210426-124955')
 data = tdt.read_block(r'C:\Users\mbj\Desktop\Uni\8. semester\Projekt\Data\Subject9-210427-112449')
 #data = tdt.read_block(r'/Users/amaliekoch/Desktop/Subject1-210914-103420')
 
-exp = 17
-channel = 1
+exp = 17 
 
-## With RS4
+## Data with RS4
 fs = data.streams.RSn1.fs # sample frequency 
 data = data.streams.RSn1.data #data 
 t = np.arange(0,len(data[0,:]),1)/fs #time vector
 
-## Without RS4
+## Data without RS4
 #fs = data.streams.Wav1.fs#RSn1.fs # sample frequency 
 #data = data.streams.Wav1.data#RSn1.data #data 
 #t = np.arange(0,len(data[0,:]),1)/fs #time vector
 
 ## Harmonics til filtrering
-harmonics = np.array([50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900])
+harmonics = np.array([50,100,150,200,250,300,350,400,450])
 
-for i in range (1):
-    notch_data = f.filt_filt_nocth_harmonic(data[i],harmonics, fs)
-    band_notch_data = f.filt_filt_but(notch_data, 'band', 5, [1,3000], fs) #10 order effective lowpass filter at 400 hz
+for i in range (len(data)): #32 kanaler
+    notch_data = f.filt_filt_nocth_harmonic(data[i],harmonics, fs) #Notch filter at 50 Hz + 8 harmonics
+    band_notch_data = f.filt_filt_but(notch_data, 'band', 5, [1,250], fs) #10 order effective bandpass filter at 1-250 hz
     
+    #Plot data in time and frequency domain
     p.plot_data_fft_log(band_notch_data,
                         t,
                         fs,
