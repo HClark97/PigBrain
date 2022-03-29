3be9f85712e8cd8e1004f25aa1c527f1454b9340
Nickolaj <nickolaj.ajay@gmail.com>
2022-03-29 15:32:16 +0200

    STFT
    STFT now generated for every epoch

--- CNN/stft.py
+++ CNN/stft.py
@@ -12,10 +12,14 @@ import matplotlib.cm as cm
 import numpy as np
 import scipy
 from scipy import signal
+from scipy.io import savemat
+import copy
 
 'Load data'
 if not 'data' in globals():
     data=l.load_mat_file()
+
+stft_dic=copy.deepcopy(data)
 
 'Definitions'
 fs = 6103.515625
@@ -26,33 +30,34 @@ i=0
 'Open up data'
 for group in range (len(groupname)): #2: nox and nonNox
     for sets in range(len(data['NonnoxERP']['block'])): #48 
+        print('Set '+ str(sets+1))
         for channel in range(len(data['NonnoxERP']['block'][0]['channel'])): #32
-            for epoch in range(len(data['NonnoxERP']['block'][0]['channel'][0]['ERPs'][0])): #100
-                if np.size(data['NonnoxERP']['block'][sets]['channel']):
-                    if np.size(data[groupname[group]]['block'][sets]['channel'][channel]['ERPs']): #If there are stimulations 
-
-                        'Get epoch'
-                        stim=data[groupname[group]]['block'][sets]['channel'][channel]['ERPs'][:,epoch]
-                    
-                        'Calculate STFT'
-                        f, t, Zxx = signal.stft(stim, fs)
-                        Zxx = np.abs(Zxx)
-                        Zxx = Zxx/np.max(Zxx)
+            if np.size(data['NonnoxERP']['block'][sets]['channel']):
+                stft_dic[groupname[group]]['block'][sets]['channel'][channel]['ERPs']=[]
+                print('Deleted entry in channel '+ str(channel+1))
+                for epoch in range(len(data['NonnoxERP']['block'][0]['channel'][0]['ERPs'][0])): #100
+                        if np.size(data[groupname[group]]['block'][sets]['channel'][channel]['ERPs']): #If there are stimulations 
+    
+                            'Get epoch'
+                            stim=data[groupname[group]]['block'][sets]['channel'][channel]['ERPs'][:,epoch]
                         
-                        'Transform to tensor'
-                        stft_tensor = nn.tensor(Zxx)
-                        
-                        #data[groupname[group]]['block'][sets]['channel'][channel]['ERPs'][:,epoch] 
-                        i += 1
-                        data[groupname[group]]['block'][sets]['channel'][channel]['ERPs'][:,:,epoch]= stft_tensor
-                        print(i)
-                        # 'Plot colormap'
-                        # plt.figure()
-                        # plt.pcolormesh(t, f, Zxx, cmap=cm.plasma)
-                        # plt.xlabel('Time [sec]', fontweight='bold')
-                        # plt.ylabel('Frequency [Hz]', fontweight='bold')    
-                        # plt.colorbar()
-                        # plt.ylim(0,300)
+                            'Calculate STFT'
+                            f, t, Zxx = signal.stft(stim, fs)
+                            Zxx = np.abs(Zxx)
+                            Zxx = Zxx/np.max(Zxx)
+                            
+                            'Transform to tensor'
+                            stft_tensor = nn.tensor(Zxx)
+                            
+                            stft_dic[groupname[group]]['block'][sets]['channel'][channel]['ERPs'].append(stft_tensor)
+                                                        
+                            # 'Plot colormap'
+                            # plt.figure()
+                            # plt.pcolormesh(t, f, Zxx, cmap=cm.plasma)
+                            # plt.xlabel('Time [sec]', fontweight='bold')
+                            # plt.ylabel('Frequency [Hz]', fontweight='bold')    
+                            # plt.colorbar()
+                            # plt.ylim(0,300)
 
-savemat("stft_tensor.mat", data) 
+#savemat("stft_tensor.mat", stft_dic)
                         
