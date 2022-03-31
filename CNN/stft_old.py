5671ff4a24c51167fd175125c1796da92c8cc6ec
Nickolaj <nickolaj.ajay@gmail.com>
2022-03-31 14:49:36 +0200

    STFT
    Added option to cut frequency axis to save space

--- CNN/stft_old.py
+++ CNN/stft_old.py
@@ -19,17 +19,26 @@ if not 'data' in globals():
 
 'Definitions'
 fs = 6103.515625
-n_fft = 256
+cutoff = 500
+nperseg = 700
+w = 'hann'
 
 for i in range(5):
     stim=data['NoxERP']['block'][5]['channel'][0]['ERPs'][:,i]
     
     'Turn stim into tensor'
-    stim_tensor=nn.tensor(stim)
+    # stim_tensor=nn.tensor(stim)
     
     'Calculate STFT'
-    stft=nn.stft(stim_tensor,n_fft,return_complex=True, window=nn.hann_window(n_fft))
-    f, t, Zxx = signal.stft(stim, fs)
+    # stft=nn.stft(stim_tensor,n_fft,return_complex=True, window=nn.hann_window(n_fft))
+    f, t, Zxx = signal.stft(stim, fs, window = w, nperseg = nperseg, noverlap=nperseg//2)
+    
+    'Cut to 500 Hz'
+    cutindex= round(cutoff/(fs/2)*len(Zxx))
+    Zxx = Zxx[0:cutindex,:] # Keep up til 500 Hz
+    f = f[0:cutindex]
+    Zxx = np.abs(Zxx)
+    Zxx = Zxx/np.max(Zxx)
     
     'Add absolute value of imaginary numbers to maginary'
     # out_real = stft[:, :, 0]
@@ -37,20 +46,12 @@ for i in range(5):
     # out_abs = nn.sqrt(out_real**2 + out_imag**2)
 
     'Arrange axis vectors'
-    py_t = np.arange(len(stft[0]))*len(stim)/float(fs)/len(stft[0])
-    py_f = np.arange(0,len(stft))*float(fs/2)/len(stft)
+    # py_t = np.arange(len(stft[0]))*len(stim)/float(fs)/len(stft[0])
+    # py_f = np.arange(0,len(stft))*float(fs/2)/len(stft)
     
-    'Plot colormap'
-    # plt.figure()
-    # plt.pcolormesh(py_t,py_f,np.abs(stft), cmap=cm.plasma)
-    # plt.xlabel('Time [sec]', fontweight='bold')
-    # plt.ylabel('Frequency [Hz]', fontweight='bold')
-    # plt.colorbar()
-    
-    
+    'Plot colormap' 
     plt.figure()
-    plt.pcolormesh(t,f,np.abs(Zxx), cmap=cm.plasma)
+    plt.pcolormesh(t, f, Zxx, cmap=cm.plasma)
     plt.xlabel('Time [sec]', fontweight='bold')
     plt.ylabel('Frequency [Hz]', fontweight='bold')    
     plt.colorbar()
-    plt.ylim(0,500)
